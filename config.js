b496ce80b810ea6e1bb9ef278fd55296b41c3967
Chee YL <cheeyanlucky@gmail.com>
2026-09-14 14:22:48 +0800

    Update leaderboard URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Device Stress Tester — public config
 window.DST_CONFIG = {
   sheetsLeaderboardUrl:
-    "https://script.google.com/macros/s/AKfycbzGTxZwwGM-cFUo703f2VaKWMONjEjVBY0wmZQl7VGGPP3ogiN8TPXGmqJ66fpH7h_w/exec",
+    "https://script.google.com/macros/s/AKfycbxS0UCQuuNovjYLVMvAUtigzvhtD6tRVY01ADskLjYf_CJiIbHmnoOtKGpIeaTRqHNr/exec",
 };
