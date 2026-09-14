45dffc6eeb24b4c361a0bd43110cb9cf40c99c8a
Chee YL <cheeyanlucky@gmail.com>
2026-09-14 10:11:12 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Device Stress Tester — public config
+window.DST_CONFIG = {
+  sheetsLeaderboardUrl:
+    "https://script.google.com/macros/s/AKfycbzGTxZwwGM-cFUo703f2VaKWMONjEjVBY0wmZQl7VGGPP3ogiN8TPXGmqJ66fpH7h_w/exec",
+};
