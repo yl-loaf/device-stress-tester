aacaad73cf8d86fac13118b7182f8ce536a58c0f
Chee YL <cheeyanlucky@gmail.com>
2026-09-15 20:02:12 +0800

    Update sheetsLeaderboardUrl in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Device Stress Tester — public config
 window.DST_CONFIG = {
   sheetsLeaderboardUrl:
-    "https://script.google.com/macros/s/AKfycbxS0UCQuuNovjYLVMvAUtigzvhtD6tRVY01ADskLjYf_CJiIbHmnoOtKGpIeaTRqHNr/exec",
+    "https://script.google.com/macros/s/AKfycbzVbh5JyiX9MnMXWXuwXWcHONnD5ztPMEeSqWnzbw5j7mI1cIUqzF0eA-h78DAiHuvH/exec",
 };
